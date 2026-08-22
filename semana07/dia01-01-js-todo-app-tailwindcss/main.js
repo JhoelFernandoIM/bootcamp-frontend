1297a464b995afe8293ca7f36b4313978fb5ef1c
JhoelFernandoIM <i2512152@continental.edu.pe>
2026-08-21 20:59:01 -0500

    borrando li con index

--- semana07/dia01-01-js-todo-app-tailwindcss/main.js
+++ semana07/dia01-01-js-todo-app-tailwindcss/main.js
@@ -20,14 +20,62 @@ let tasks = [
     }
 ]
 
+taskInput.addEventListener('keydown', (event) => {
+    // console.log('agregando tarea...', event.key, event.keyCode)
+    if (event.key === 'Enter') {
+        const newTask = {
+            title: event.target.value,
+            completed: false
+        }
+
+        console.log(newTask)
+
+        tasks.push(newTask)
+
+        renderTasks(tasks)
+
+        taskInput.value = ''
+    }
+
+})
+
 function renderTasks(tasks = []) {
     let lista = ''
 
-    tasks.forEach((task) => {
-        lista = lista + `<li>${task.title}</li>`
+    tasks.forEach((task, index) => {
+        lista = lista + `
+            <li class="flex justify-center items-center gap-4 py-1">
+            <input
+                type="checkbox"
+                ${task.completed ? 'checked' : ''}
+            >
+            <div class="w-full">
+                ${task.title}
+            </div>
+            <button
+                class="task__clear border border-red-700 font-medium text-sm px-2 py-1 text-red-500 rounded-lg hover:text-white hover:bg-red-700 duration-300 cursor-pointer"
+                onclick = "removeTask(${index})"
+            >
+                Borrar
+            </button>
+            </li>
+            
+            `
     });
 
     taskList.innerHTML = lista
+}
+
+function removeTask(selectedIndex) {
+    console.log(selectedIndex)
+
+    //TODO: Remover el indice selccionado del arreglo tasks y posteriormente actualizar la lista de tareas con la funcion renderTak
+
+    const modifiedTasks = tasks.filter((task, index) => index !== selectedIndex)
+    
+    tasks = modifiedTasks
+
+    renderTasks(tasks)
 }
 
 renderTasks(tasks)
